d2e4ae146092b470ddde56c253701034a5ca8928
Kristjan Vedel <vedel@rushstreetinteractive.com>
2022-11-16 13:56:06 +0200

    simple betting logic

---
+++ src/main/java/org/leanpoker/player/BettingUtil.java
@@ -0,0 +1,34 @@
+package org.leanpoker.player;
+
+public class BettingUtil {
+
+	public static int bet(GameState state, Action action) {
+		org.leanpoker.player.GameState.Player p = findSelf(state);
+		int currentBuyIn = state.current_buy_in;
+		int minimumraise = state.minimum_raise;
+		int currentBet = p.bet;
+		switch (action) {
+		case CHECK_FOLD:
+			return 0;
+		case ALL_IN:
+			return p.stack;
+		case CALL:
+			return currentBuyIn - currentBet;
+		case RAISE:
+			return currentBuyIn - currentBet + minimumraise;
+		case RAISE_X2:
+			return currentBuyIn - currentBet + (2 * minimumraise);
+		}
+		return 0;
+	}
+
+	private static org.leanpoker.player.GameState.Player findSelf(GameState gs) {
+		for (org.leanpoker.player.GameState.Player player : gs.players) {
+			if (player.name.equals("IamNotVeryGoodAtThis")) {
+				return player;
+			}
+		}
+		throw new RuntimeException("Self not found");
+
+	}
+}
