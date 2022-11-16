7db36b2219b785f7cff184e1a3d8324074d90e60
Kristjan Vedel <vedel@rushstreetinteractive.com>
2022-11-16 11:58:58 +0200

    gamestate parsing

---
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -0,0 +1,65 @@
+package org.leanpoker.player;
+
+import java.util.List;
+
+public class GameState {
+    public String tournament_id;
+    public String game_id;
+    public int round;
+    public int bet_index;
+    public int small_blind;
+    public int current_buy_in;
+    public int pot;
+    public int minimum_raise;
+    public int dealer;
+    public int orbits;
+    public int in_action;
+    public List<Player> players;
+    public List<CommunityCard> community_cards;
+
+	public static class CommunityCard{
+	    public String rank;
+	    public String suit;
+		@Override
+		public String toString() {
+			return "CommunityCard [rank=" + rank + ", suit=" + suit + "]";
+		}
+	    
+	}
+
+	public static class HoleCard{
+	    public String rank;
+	    public String suit;
+		@Override
+		public String toString() {
+			return "HoleCard [rank=" + rank + ", suit=" + suit + "]";
+		}
+	    
+	}
+
+	public static class Player{
+	    public int id;
+	    public String name;
+	    public String status;
+	    public String version;
+	    public int stack;
+	    public int bet;
+	    public List<HoleCard> hole_cards;
+
+	    @Override
+		public String toString() {
+			return "Player [id=" + id + ", name=" + name + ", status=" + status + ", version=" + version + ", stack="
+					+ stack + ", bet=" + bet + ", hole_cards=" + hole_cards + "]";
+		}	    
+	}
+
+	@Override
+	public String toString() {
+		return "GameState [tournament_id=" + tournament_id + ", game_id=" + game_id + ", round=" + round
+				+ ", bet_index=" + bet_index + ", small_blind=" + small_blind + ", current_buy_in=" + current_buy_in
+				+ ", pot=" + pot + ", minimum_raise=" + minimum_raise + ", dealer=" + dealer + ", orbits=" + orbits
+				+ ", in_action=" + in_action + ", players=" + players + ", community_cards=" + community_cards + "]";
+	}
+
+	
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,28 @@
 package org.leanpoker.player;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Player {
+	final static Logger logger = LoggerFactory.getLogger(Player.class);
 
-    static final String VERSION = "Default Java folding player";
+	static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonNode request) {
-        return 0;
-    }
+	static final ObjectMapper mapper = new ObjectMapper();
 
-    public static void showdown(JsonNode game) {
-    }
+	public static int betRequest(JsonNode request) {
+		try {
+			GameState state = mapper.treeToValue(request, GameState.class);
+			logger.info("state {}", state);
+		} catch (Exception e) {
+			logger.error("gameState", e);
+		}
+		return 0;
+	}
+
+	public static void showdown(JsonNode game) {
+	}
 }
