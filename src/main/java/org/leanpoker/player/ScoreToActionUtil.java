fa432a5241fd7b01f04d47a9b4c3260465a26f97
Cardo Eggert <eggert@rushstreetinteractive.com>
2022-11-16 16:22:43 +0200

    Refactor with high pair decision change

--- src/main/java/org/leanpoker/player/ScoreToActionUtil.java
+++ src/main/java/org/leanpoker/player/ScoreToActionUtil.java
@@ -11,7 +11,7 @@ public class ScoreToActionUtil {
     public static Action scoreToActionWithMoreCards(List<GameState.HoleCard> cards, List<GameState.HoleCard> playerCards, List<GameState.HoleCard> communityCards, int score, int buyInAmount) {
         return switch (score) {
             case 0 -> highCardAction(cards, buyInAmount);
-            case 1 -> pairCardAction(cards, playerCards, communityCards);
+            case 1 -> pairCardAction(cards, playerCards, communityCards, buyInAmount);
             case 2 -> // Two Pairs
                     Action.RAISE; // Three of a kind
             case 3, 4 -> // Straight
@@ -37,11 +37,25 @@ public class ScoreToActionUtil {
     private static Action pairCardAction(
             List<GameState.HoleCard> cards,
             List<GameState.HoleCard> playerCards,
-            List<GameState.HoleCard> communityCards) {
+            List<GameState.HoleCard> communityCards,
+            int buyInAmount) {
         boolean communityHasPair = communityHasPairs(communityCards);
         boolean playerHasPair = playerCards.get(0).rank.equals(playerCards.get(1).rank);
         if (!communityHasPair && !playerHasPair) {
-            return Action.RAISE;
+            final String rankingPair = communityCards
+                    .stream()
+                    .filter(communityCard -> playerCards.stream().anyMatch(playerCard -> playerCard.rank.equals(communityCard.rank)))
+                    .findAny()
+                    .get()
+                    .rank;
+            boolean rankingPairHigh = HIGH_CARDS.contains(rankingPair);
+            if (rankingPairHigh) {
+                return Action.CALL;
+            }
+            if (buyInAmount < 100) {
+                return Action.CHECK_FOLD;
+            }
+            return Action.CALL;
         }
         if (playerHasPair || cards.size() < 7) {
             boolean highCardPair = HIGH_CARDS.contains(playerCards.get(0).rank);
@@ -72,41 +86,44 @@ public class ScoreToActionUtil {
         cards.addAll(playerCards);
         cards.addAll(communityCards);
         if (cards.size() == 2) {
-            boolean isPair = cards.get(0).rank.equals(cards.get(1).rank);
-            boolean sameSuit = cards.get(0).suit.equals(cards.get(1).suit);
-            boolean firstCardHigh = HIGH_PREFLOP_CARDS.contains(cards.get(0).rank);
-            boolean secondCardHigh = HIGH_PREFLOP_CARDS.contains(cards.get(1).rank);
-            if (isPair) {
-                if (firstCardHigh && secondCardHigh) {
-                    return Action.ALL_IN;
-                }
-                return Action.RAISE_X10;
-            }
-            if (firstCardHigh && secondCardHigh) {
-                return Action.ALL_IN;
-            } else if (firstCardHigh || secondCardHigh) {
-                if (sameSuit) {
-                    return Action.RAISE;
-                }
-                return Action.CALL;
-            } else {
-                boolean firstCardLow = LOW_CARDS.contains(cards.get(0).rank);
-                boolean secondCardLow = LOW_CARDS.contains(cards.get(1).rank);
-                if (!firstCardLow && !secondCardLow) {
-                    if (buyInAmount > 50) {
-                        return Action.CHECK_FOLD;
-                    }
-                    return Action.CALL;
-                } else if (firstCardLow && secondCardLow) {
-                    return Action.CHECK_FOLD;
-                }
-                if (sameSuit) {
-                    return Action.CALL;
-                }
-                return Action.CHECK_FOLD;
-            }
+            return preFlopAction(playerCards, buyInAmount);
         }
         return scoreToActionWithMoreCards(cards, playerCards, communityCards, score, buyInAmount);
     }
 
+    public static Action preFlopAction(List<GameState.HoleCard> cards, int buyInAmount) {
+        boolean isPair = cards.get(0).rank.equals(cards.get(1).rank);
+        boolean sameSuit = cards.get(0).suit.equals(cards.get(1).suit);
+        boolean firstCardHigh = HIGH_PREFLOP_CARDS.contains(cards.get(0).rank);
+        boolean secondCardHigh = HIGH_PREFLOP_CARDS.contains(cards.get(1).rank);
+        if (isPair) {
+            if (firstCardHigh && secondCardHigh) {
+                return Action.ALL_IN;
+            }
+            return Action.RAISE_X10;
+        }
+        if (firstCardHigh && secondCardHigh) {
+            return Action.ALL_IN;
+        } else if (firstCardHigh || secondCardHigh) {
+            if (sameSuit) {
+                return Action.RAISE;
+            }
+            return Action.CALL;
+        } else {
+            boolean firstCardLow = LOW_CARDS.contains(cards.get(0).rank);
+            boolean secondCardLow = LOW_CARDS.contains(cards.get(1).rank);
+            if (!firstCardLow && !secondCardLow) {
+                if (buyInAmount > 50) {
+                    return Action.CHECK_FOLD;
+                }
+                return Action.CALL;
+            } else if (firstCardLow && secondCardLow) {
+                return Action.CHECK_FOLD;
+            }
+            if (sameSuit) {
+                return Action.CALL;
+            }
+            return Action.CHECK_FOLD;
+        }
+    }
 }
